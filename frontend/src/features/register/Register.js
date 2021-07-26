5c4e8a979e2dff64f093a415f214ee5edae981f3
hjk216 <62774444+hjk216@users.noreply.github.com>
2021-07-26 16:34:46 -0400

    cookie change

--- frontend/src/features/register/Register.js
+++ frontend/src/features/register/Register.js
@@ -20,7 +20,7 @@ export default function Register() {
         register({username: username, password: password})
         .unwrap()
         .then((payload) => {
-            setCookie('token', payload.token, {secure: true, sameSite: true, maxAge: 36000})
+            setCookie('token', payload.token, {secure: true, sameSite: true, expires: 36000})
             window.location.reload();
         })
     }
